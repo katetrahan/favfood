3613ee9b1b2fc306c719a2882cf8e862b55345f7
kathrynceciliatrahan@gmail.com and ryanglenferris@gmail.com <ryanglenferris@gmail.com@>
2017-12-04 10:09:40 -0800

    update favorite food array logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,14 @@ $(document).ready(function() {
     var bestFood = $("input#favfood").val();
     var secondFav=$("input:radio[name=operator]:checked").val();
     var foodarray= [foodPreference, bestFood, secondFav];
-    var shortarray=foodarray[0].concat("and "+foodarray[1]);
+    // var shortarray=foodarray[0].concat("and "+foodarray[1]);
+    var foodArrayOne = foodarray[0];
+    var foodArrayTwo = foodarray[1];
 
 
-    $(".shortarrayoutput").text(shortarray);
+    // $(".shortarrayoutput").text(shortarray);
+    $(".shortarrayoutput").append("<li>"+foodArrayOne+"</li>");
+    $(".shortarrayoutput").append("<li>"+foodArrayTwo+"</li>");
 
 
   });
